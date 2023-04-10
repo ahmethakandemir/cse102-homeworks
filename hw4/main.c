1efe1fd086aa56de5aae32dd19e79d654f4f9277
ahmethakandemir <72705464+ahmethakandemir@users.noreply.github.com>
2023-04-10 21:14:23 +0300

    hw4 commited

--- hw4/main.c
+++ hw4/main.c
@@ -1,21 +1,93 @@
 #include <stdio.h>
+#include <time.h>
+
 int menuing();
-float selecting();
+int billing();
+float total(float sum);
+void billtoterminal();
 
 FILE * fmenu;
 FILE * fbill;
 
 int main(){
     menuing();
-    selecting();
+    billing();
 
 }
 
-float selecting(){
-
-    fbill = fopen("bill.txt","w");
+void billtoterminal(){
+    fbill = fopen("bill.txt","r");
+    int c;
+    printf("\n\nHere is your bill: \n\n");
+    while(1){
+        c = fgetc(fbill);
+        printf("%c",c);
+        if(c == EOF){
+            break;
+        }
+    }
+    printf("\n");
     fclose(fbill);
+}
 
+
+float total(float sum){
+    char student;
+    float disc;
+    int flag = 0;
+    float vat;
+    vat = sum * 18 / 100;
+    sum = sum + vat; //VAT value calculated before student discount, and added to price payable.
+    fbill = fopen("bill.txt","a");
+    fprintf(fbill,"\nVAT(%%18):                    +%.2f",vat);
+    while(!flag){
+
+        printf("Are you student? (Y/N):");
+        scanf(" %c",&student);
+        switch (student)
+        {
+        case 'y':
+        case 'Y':
+            disc = sum * 12.5 / 100;
+            sum -= disc;
+            fprintf(fbill,"\nStudent Discount:            -%.2f",disc);
+            flag = 1;
+            break;
+        case 'N':
+        case 'n':
+            flag = 1;
+            break;
+        default:
+            printf("Invalid input!");
+            break;
+        }
+    }
+
+
+
+    fprintf(fbill,"\n---------------------------------------\n");
+    fprintf(fbill,"Price Payable:                %.2f",sum);
+    
+    fclose(fbill);
+    return sum;
+}
+
+
+
+int billing(){
+    fbill = fopen("bill.txt","w");
+    fprintf(fbill,"210104004055           ");
+    time_t t;
+    t = time(NULL);
+    char arr[30];
+    strftime(arr,sizeof(arr),"%d.%m.%Y/%H:%M",localtime(&t));
+    fprintf(fbill,"%s",arr);
+    fprintf(fbill,"\n---------------------------------------\n");
+    fprintf(fbill,"Product                       Price\n");
+    fprintf(fbill,"---------------------------------------");
+    fprintf(fbill,"\n");
+    fclose(fbill);
+    float sum = 0;
     float price;
     int c,i,servings;
     while(1){
@@ -24,6 +96,7 @@ float selecting(){
         while (choose < 1 || choose > 10){
             printf("\nPlease choose a product (1-10): ");
             scanf("%d",&choose);
+            choose += 2;
             if((choose < 1 || choose > 10)){
                 printf("Please enter a valid selection!!\n");
             }
@@ -34,11 +107,7 @@ float selecting(){
         printf("How many servings do you want?: ");
         scanf("%d",&servings);
 
-        if(servings == 0){
-            return 0;
-        }
-        else{
-
+        if(servings != 0){
             fbill = fopen("bill.txt","a+");
             fmenu = fopen("menu.txt","r");
             for(i = 0; i < choose - 1;i++){
@@ -66,13 +135,21 @@ float selecting(){
             fclose(fmenu);
 
             price *= servings;
-            for(i = 0; i < 12 - boslukcu ;i++){
+            sum += price;
+            for(i = 0; i < 30 - boslukcu ;i++){
                 fprintf(fbill," ");
             }
             fprintf(fbill,"%.2f\n",price);
             fclose(fbill);
         }
+        else{
+            total(sum);
+            billtoterminal();
+            //fprintf(fbill,"%.2f",total(student,sum));
+            return 0;
+        }
     }
+    
     return 1;
 }
 
@@ -80,28 +157,29 @@ float selecting(){
 
 int menuing(){
     fmenu = fopen("menu.txt","r");
-    int i = 1;
+    int i = 1,k = 0;
     printf("Yemek Listesi:\n");
     printf("1. ");
-    while (1){   
-        while(1){
-            int c = fgetc(fmenu);
-            if(c == EOF){
-                printf("\n");
-                return 0;
-            }
-            if((c >= 48 && c <= 57) || c == 46){}
-            else{
-                printf("%c",c);
-            }
-            if(c == 10){
+
+    while(1){
+        int c = fgetc(fmenu);
+
+        if(c == EOF){
+            printf("\n");
+            return 0;
+        }
+        if(((c >= 48 && c <= 57) || c == 46) || k < 2){}
+        else{
+            printf("%c",c);
+        }
+        if(c == 10){
+            k++;
+            if(k > 2){
                 i++;
                 printf("%d. ",i);
             }
         }
-
     }
-
 
     fclose(fmenu);
 
