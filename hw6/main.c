3b741f57a81abd6721dacfa45befa8d30ea98548
Ahmet Hakan Demir <72705464+ahmethakandemir@users.noreply.github.com>
2023-04-23 14:48:21 +0300

    up

--- hw6/main.c
+++ hw6/main.c
@@ -15,11 +15,28 @@ int current_stock[100];
 void puttingArrays(){
     products = fopen("products.txt","r");
     stocks = fopen("stocks.txt","r");
-    int c,productLines = 0,pid;
-    fscanf(products,"%d",&pid);
+    int c,productLines = 0,i = 0,letter = 0,letindex = 0;
     while (1)   // getting the line count
     {
+
+        fscanf(products,"%d",&pid[i]);
+        fgetc(products);
+        fscanf(products,"%c",&type[i]);
+        fgetc(products); // c = ','
+
+
+        while ((letter = fgetc(products)) != ',' && letter != '\n' && letter != EOF) {
+            name[i][letindex] = letter;
+            letindex++;
+            if (letindex + 1 == 8) break;  /* prevent writing beyond array */
+        }
+        letindex = 0;
+        fgetc(products);
+        fscanf(products,"%s",brand[i]);
+        fgetc(products);
+        fscanf(products,"%lf",&price[i]);
         c = fgetc(products);
+
         if(c == EOF){
             productLines++;
             break;
@@ -27,9 +44,10 @@ void puttingArrays(){
         else if(c == '\n'){
             productLines++;
         }
+        i++;
     }
-    printf("%d",productLines);
 
+    printf("%s",name[i]);
 
     fclose(products);
     fclose(stocks);
