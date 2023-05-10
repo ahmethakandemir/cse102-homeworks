662b5b1a1c1112910676e31fbb510b7e68456447
ahmethakandemir <72705464+ahmethakandemir@users.noreply.github.com>
2023-05-10 18:22:23 +0000

    up

--- hw7/main.c
+++ hw7/main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
 const char dictionary[20] = "dictionary.txt";
 const char inputtxt[20] = "input.txt";
 const char ignoretxt[20] = "ignore.txt";
 
 #define MAX_WORD_SIZE 13 // one more byte for eof char.
 #define MAX_WORD_COUNT 100
+
 
 int numwords(){
     FILE * dict;
@@ -18,7 +22,6 @@ int numwords(){
     fclose(dict);
     return numword;
 }
-
 int vectorsize(){
     FILE * dict;
     dict = fopen("dictionary.txt","r");
@@ -34,6 +37,77 @@ int vectorsize(){
     fclose(dict);
     return vector;
 }
+
+
+
+void vectorAdding(const char *  file_name, char dict[][MAX_WORD_SIZE],float vecs[numwords()][vectorsize()]){
+
+	int i,j=0,c=0,numss,vectorss;
+    char line[100];
+	FILE *fpdict;
+    fpdict = fopen("dictionary.txt","r");
+    numss = numwords();
+    fclose(fpdict);
+    vectorss = vectorsize();
+	fpdict=fopen(file_name,"r");
+	/*while (c != 10){
+        c = fgetc(fpdict);
+        //printf("%c",c);
+    }*/
+    fgets(line,100,fpdict);
+	for(i=0; i< numss; i++){
+		fscanf(fpdict,"%s", dict[i]);
+		for(j=0; j<vectorss; j++){
+			fscanf(fpdict, "%f", &vecs[i][j]);
+		}	
+	}
+
+	dict[numss][0] = '-';
+	dict[numss][1] = '\0';/*
+    for(i=0; i<numss; i++){
+		for(j=0; j<vectorss; j++){
+			//printf("%.5f ", vecs[i][j]);
+		}
+		printf("\n");
+	}*/
+    for(i = 0;i < numwords(); i++){
+        printf("%s ",dict[i]);
+    }
+	
+}
+
+
+double dissimilarity(char * w1, char * w2, char dict[][MAX_WORD_SIZE], float threshold, float vecs[numwords()][vectorsize()]){
+
+    int i,tempint;
+    for(i = 0; i < numwords();i++){
+        if(strcmp(w1,dict[i]) == 0){
+            break;
+        }
+    }
+    float temp = 100,sum = 0;
+    for(int k = 0;k < numwords();k++){
+        sum = 0;
+        for(int j = 0;j < vectorsize();j++){
+            sum += pow(vecs[k][j] - vecs[i][j],2);
+        }
+        sum = sqrt(sum);
+        printf("\n%f",sum);
+        if((sum < temp)&&(sum != 0)){
+            temp = sum;
+            tempint = k;
+        }
+        if(threshold < temp){
+            return -1;
+        }
+    }
+    for(int m = 0;m < MAX_WORD_SIZE; m++){
+        w2[m] = dict[tempint][m];
+    }
+    printf("\n%s\n",w2);
+    return 0;
+}
+
 int read_dict(const char *  file_name, char dict[][MAX_WORD_SIZE]){
 	int i,j=0,c=0,numss,vectorss;
 	FILE *fpdict;
@@ -60,18 +134,21 @@ int read_dict(const char *  file_name, char dict[][MAX_WORD_SIZE]){
 	//testing vecs
 	for(i=0; i<numss; i++){
 		for(j=0; j<vectorss; j++){
-			printf("%.1f ", vecs[i][j]);
+			//printf("%.1f ", vecs[i][j]);
 		}
-		printf("\n");
+		//printf("\n");
 	}
     for(i = 0;i < numwords(); i++){
-        printf("%s ",dict[i]);
+        //printf("%s ",dict[i]);
     }
+
 	return numss;	
 }
+
 int read_text(const char * text_file, const char * ignore_file, char words[][MAX_WORD_SIZE]){
     FILE * fpinput;
     FILE * fpignore;
+    
     char ignore[100][MAX_WORD_SIZE];
     fpignore = fopen(ignore_file,"r");
     int c,counter = 0;
@@ -93,10 +170,12 @@ int read_text(const char * text_file, const char * ignore_file, char words[][MAX
         i++;
     }
     fclose(fpignore);
+    
     i = 0;
     int k;
     fpinput = fopen(text_file,"r");
     while(1){
+        
         fscanf(fpinput,"%s",words[i]);
         c = fgetc(fpinput);
         if(c == -1){
@@ -128,17 +207,13 @@ int read_text(const char * text_file, const char * ignore_file, char words[][MAX
                     i++;
                     int e,index;
                     for(e = k,index = 0;e < MAX_WORD_SIZE + 1;e++,index++){
-                        printf("i is : %d\n",i);
-                        printf("char is :%c\n",words[i - 1][e + 3]);
+                        //printf("i is : %d\n",i);
+                        //printf("char is :%c\n",words[i - 1][e + 3]);
                         words[i][index] = words[i - 1][e + 3];
+                        
                     }
-                    for(int m = k; m < MAX_WORD_SIZE + 1; m++){
-                        words[i - 1][m] = '\0';
-                    }
+                    words[i-2][e+3]= '\0';
                         //printf("1. index is : %s\n",words[1]);
-
-                    
-                    
                 }
                 if(!((words[i][k] >= 65 && words[i][k] <= 90) || (words[i][k] >= 97 && words[i][k] <= 122) || (words[i][k] >= 48 && words[i][k] <= 57))){
                     words[i][k] = '\0';
@@ -151,23 +226,106 @@ int read_text(const char * text_file, const char * ignore_file, char words[][MAX
     }
     fclose(fpinput);
     //printf("%d",i);
-    for(int l = 0; l < 20;l++){
-        //printf("%s",words[l]);
+
+    
+
+
+    for(int l = 0; l < i+2;l++){
+        for(int b = 0;b < counter;b++){
+            if(strcmp(ignore[b],words[l]) == 0){
+                for(int t = l; t < i;t++){
+                    for(int n = 0;n < MAX_WORD_SIZE;n++){
+                        words[t][n] = words[t + 1][n];
+                    }
+                }
+            } 
+        }
     }
-    printf("word is : %s\n",words[2]);
+
+    for(int l = 0; l < i+2;l++){
+        //printf("%s ",words[l]);
+    }
+
+    //printf("\n\n%s\n",words[6]);
+    //printf("word is : %s\n",words[2]);
     return i;
 }
 
+void occurence(char inputt[100][MAX_WORD_SIZE],char words[300000][MAX_WORD_SIZE],int enternum,int counter[100]){
+
+    char w1[MAX_WORD_SIZE];
+    int i,k;
+    int wordsize = read_text(inputtxt,ignoretxt,words);
+    for(i = 0; i < wordsize; i++){
+        for(int j = 0;j < enternum; j++){
+            if(strcmp(inputt[j],words[i]) == 0){
+            counter[j]++;
+            }
+        }
+    }
+    //printf("%d",count);
+    return;
+}
+
+
 int main(){
+    FILE * fptemp = fopen("temp.txt","w");
     //double vectors[numwords()][vectorsize()];
-    //char dict[numwords() + 1][MAX_WORD_SIZE];
+   
+    int x;
+    x=numwords();
+     printf("\n%d",x);
+    char dict[1001][MAX_WORD_SIZE];
     char words[300000][MAX_WORD_SIZE];
+    char inputtedWord;
+    char inputt[100][MAX_WORD_SIZE];
+    int counter[100] = {0};
+    float vecs[numwords()][vectorsize()];
+    char w1[MAX_WORD_SIZE] = {"lightly"};
+    char w2[MAX_WORD_SIZE];
 
+    printf("Enter text (press enter to exit):\n");
+    while ((inputtedWord = getchar()) != EOF) {
+        putc(inputtedWord, fptemp);
+        if (inputtedWord == '\n') {
+            break;
+        }
+    }
+    fclose(fptemp);
+    fptemp = fopen("temp.txt","r");
+    int c,i = 0,enternum = 0;
+    while(1){
+        c = fgetc(fptemp);
+        if(c != EOF) ungetc(c,fptemp);
+        else break;
+        fscanf(fptemp,"%s",inputt[i]);
+        enternum++;
+        i++;
 
+    }
 
-    //read_dict(dictionary,dict);
-    read_text(inputtxt,ignoretxt,words);
+    // read_dict(dictionary,dict);
+    // read_text(inputtxt,ignoretxt,words);
+    vectorAdding(dictionary,dict,vecs);
+    // occurence(inputt,words,enternum,counter);
+    dissimilarity(w1,w2,dict,6,vecs);//////////////////////////////////////////////////////////////////////////////
+    for(i = 0;i < enternum - 1;i++){
+        if(counter[i] != 0){
+            printf("occurence is : %d\n",counter[i]);
+        }
+        else{
+            printf("closest word's occurence is :\n");
+        }
+    }
 
+    for(int i = 0;i < numwords();i++){
+        for(int k = 0;k< vectorsize();k++){
+            //printf("%f  ",vecs[i][k]);
+        }
+        //printf("\n");
+    }
+    
 
+    //system("rm temp.txt");
     return 0;
 }
