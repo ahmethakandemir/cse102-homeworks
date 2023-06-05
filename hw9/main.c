673c9c89dc7a26d3d5eeb8b125de9374e7093186
ahmethakandemir <72705464+ahmethakandemir@users.noreply.github.com>
2023-06-05 16:22:56 +0300

    Update main.c

--- hw9/main.c
+++ hw9/main.c
@@ -86,7 +86,7 @@ void draw_board(struct boardstruct*** board, struct snakestruct* snake, int snak
 }
 
 
-void move(struct boardstruct*** board, struct snakestruct* snake, int snakeSize) {
+void move(struct boardstruct*** board, struct snakestruct* snake, int snakeSize,char *lastDirection) {
     char direction;
     while (1) { // while loop for the iteration of prompting until the user enters a legal move.
         printf("\nEnter your move: ");
@@ -99,49 +99,67 @@ void move(struct boardstruct*** board, struct snakestruct* snake, int snakeSize)
             printf("\nPlease enter a legal move!!!:");
         }
     }
-
+    //printf("last direction: %c\n",*lastDirection);
+    ////////////  LAST DIRECTION AYARLA //////////////////////////////////////////////////
     switch (direction) {
         case 'w':
-            snake[0].y -= 1;
+            if(*lastDirection == 's'){
+                snake[0].y -= 1;
+                *lastDirection = direction;
+            }
             break;
         case 's':
-            snake[0].y += 1;
+            if(*lastDirection != 'w'){
+                snake[0].y += 1;
+                *lastDirection = direction;
+            }
             break;
         case 'a':
-            snake[0].x -= 1;
+           if(*lastDirection != 'd'){
+               snake[0].x -= 1;
+               *lastDirection = direction;
+            }
             break;
         case 'd':
-            snake[0].x += 1;
+            if(*lastDirection != 'a'){
+                snake[0].x += 1;
+                *lastDirection = direction;
+            }
             break;
         default:
             break;
     }
-}
 
-int update(struct boardstruct*** board, struct snakestruct* snake, int* snakeSize) {
+    
+
+
+}
+int update(struct boardstruct*** board, struct snakestruct** snake, int* snakeSize) {
     static int moveCounter = 0;  // Static variable to keep track of the number of moves
 
     // Check if the snake ate a bait
     for (int i = 0; i < 10; i++) {
         for (int k = 0; k < 10; k++) {
             if (board[i][k]->type == 'b') {
-                if ((snake[0].x == k) && (snake[0].y == i)) {
+                if (((*snake)[0].x == k) && ((*snake)[0].y == i)) {
                     // Increase the snake's length and generate new bait
                     board[i][k]->type = 'e';
                     (*snakeSize)++;
+                    (*snake) = (struct snakestruct*)realloc(*snake, (*snakeSize) * sizeof(struct snakestruct));
                     int xbait, ybait;
                     while (1) {
-                        int flag = 0;
                         xbait = rand() % 10;
                         ybait = rand() % 10;
-                        for(int k = 0; k < *snakeSize;k++){
-                            if((xbait == snake[k].x) && (ybait == snake[k].y)){
-                                flag = 1;
+
+                        // Check if the new bait doesn't overlap with snake or existing bait
+                        int isOverlap = 0;
+                        for (int k = 0; k < *snakeSize; k++) {
+                            if ((xbait == (*snake)[k].x) && (ybait == (*snake)[k].y)) {
+                                isOverlap = 1;
                                 break;
                             }
                         }
-                        // Check if the new bait doesn't overlap with snake or existing bait
-                        if (!(board[xbait][ybait]->type == 'b') && !(board[xbait][ybait]->type == 'o') && (flag == 0)) {
+                        if (!isOverlap) {
                             break;
                         }
                     }
@@ -154,8 +172,8 @@ int update(struct boardstruct*** board, struct snakestruct* snake, int* snakeSiz
 
     // Update the coordinates of the snake's body
     for (int i = *snakeSize - 1; i > 0; i--) {
-        snake[i].x = snake[i - 1].x;
-        snake[i].y = snake[i - 1].y;
+        (*snake)[i].x = (*snake)[i - 1].x;
+        (*snake)[i].y = (*snake)[i - 1].y;
     }
 
     moveCounter++;  // Increment the move counter
@@ -173,22 +191,64 @@ int update(struct boardstruct*** board, struct snakestruct* snake, int* snakeSiz
 
         // Generate new obstacles if the maximum limit is not reached
         if (numObstacles < 3) {
-            int xobs, yobs;
             while (1) {
-                xobs = rand() % 10;
-                yobs = rand() % 10;
+                int xobs = rand() % 10;
+                int yobs = rand() % 10;
+
                 // Check if the obstacle doesn't overlap with snake or bait
-                if (!(board[xobs][yobs]->type == 'b') && !(board[xobs][yobs]->type == 'o')) {
+                int isOverlap = 0;
+                for (int k = 0; k < *snakeSize; k++) {
+                    if (((*snake)[k].x == xobs) && ((*snake)[k].y == yobs)) {
+                        isOverlap = 1;
+                        break;
+                    }
+                }
+                if (!isOverlap && (board[xobs][yobs]->type != 'b')) {
+                    board[xobs][yobs]->type = 'o';
+                    board[xobs][yobs]->value = rand() % 9 + 1;
                     break;
                 }
             }
-            board[xobs][yobs]->type = 'o';
-            board[xobs][yobs]->value = rand() % 9 + 1;
+        }
+    }
+
+    // Check if the bait overlaps with the snake's tail and regenerate if necessary
+    if (board[(*snake)[0].y][(*snake)[0].x]->type == 'b') {
+        int isOverlap = 0;
+        for (int k = 1; k < *snakeSize; k++) {
+            if (((*snake)[k].x == (*snake)[0].x) && ((*snake)[k].y == (*snake)[0].y)) {
+                isOverlap = 1;
+                break;
+            }
+        }
+        if (isOverlap) {
+            // Generate new bait
+            int xbait, ybait;
+            while (1) {
+                xbait = rand() % 10;
+                ybait = rand() % 10;
+
+                // Check if the new bait doesn't overlap with snake or existing bait
+                int isOverlap = 0;
+                for (int k = 0; k < *snakeSize; k++) {
+                    if ((xbait == (*snake)[k].x) && (ybait == (*snake)[k].y)) {
+                        isOverlap = 1;
+                        break;
+                    }
+                }
+                if (!isOverlap) {
+                    break;
+                }
+            }
+            board[(*snake)[0].y][(*snake)[0].x]->type = 'e';
+            board[xbait][ybait]->type = 'b';
         }
     }
 
     return 1;
 }
+
+
 
 int check_status(struct boardstruct*** board, struct snakestruct* snake, int snakeSize) {
     if (snake[0].x < 0 || snake[0].y < 0 || snake[0].x > 9 || snake[0].y > 9) {
@@ -216,25 +276,25 @@ int check_status(struct boardstruct*** board, struct snakestruct* snake, int sna
     }
     return 0;
 }
-
 void play(struct boardstruct*** board) {
     int snakeSize = 2;
-    struct snakestruct* snake = (struct snakestruct*)malloc(snakeSize * sizeof(struct snakestruct));
+    struct snakestruct* snake = (struct snakestruct*)malloc(sizeof(struct snakestruct));
     snake[0].x = 0;
     snake[0].y = 0;
-
+    char lastDirection = ' ';
     while (1) {
         draw_board(board, snake, snakeSize);
-        printf("\nsnake size is: %d\n",snakeSize - 1);
-        move(board, snake, snakeSize);
-        if (check_status(board,snake, snakeSize)) {
+        printf("\nsnake size is: %d\n", snakeSize - 1);
+        move(board, snake, snakeSize,&lastDirection);
+        if (check_status(board, snake, snakeSize)) {
             printf("Game Over! You lost.\n");
             break;
         }
-        update(board,snake,&snakeSize);
+        update(board, &snake, &snakeSize);
     }
     free(snake);
 }
+
 
 int main() {
     srand(time(NULL));
