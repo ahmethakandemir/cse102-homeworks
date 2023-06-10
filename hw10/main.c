c443f86c5aa390e89f70d7f0932d78bd85342436
ahmethakandemir <72705464+ahmethakandemir@users.noreply.github.com>
2023-06-10 14:00:12 +0300

    .

--- hw10/main.c
+++ hw10/main.c
@@ -20,7 +20,17 @@ typedef struct students {
 } student;
 
 student* students = NULL;
-
+void printBooks() {
+    book* currentBook = books;
+    while (currentBook != NULL) {
+        printf("ISBN: %s\n", currentBook->isbn);
+        printf("Title: %s\n", currentBook->title);
+        printf("Author: %s\n", currentBook->author);
+        printf("Publication Year: %d\n", currentBook->publicationYear);
+        printf("-----------------------------\n");
+        currentBook = currentBook->next;
+    }
+}
 void addBook(char* isbn, char* title, char* author, int publicationYear, int method) {
     // Create a new book
     book* newBook = (book*)malloc(sizeof(book));
@@ -35,18 +45,32 @@ void addBook(char* isbn, char* title, char* author, int publicationYear, int met
     if (books == NULL) {
         books = newBook;
     } else {
-        // Find the last book in the linked list
-        book* currentBook = books;
-        while (currentBook->next != NULL) {
-            currentBook = currentBook->next;
+        // Add the new book at the beginning or end based on the method chosen
+        if (method == 0) {
+            // FIFO - Add the new book at the end
+            book* currentBook = books;
+            while (currentBook->next != NULL) {
+                currentBook = currentBook->next;
+            }
+            currentBook->next = newBook;
+        } else if (method == 1) {
+            // LIFO - Add the new book at the beginning
+            newBook->next = books;
+            books = newBook;
         }
-
-        // Assign the new book as the next book of the last book
-        currentBook->next = newBook;
     }
 }
 
-void txttoll(){
+
+void txttoll() {
+    // Free memory for existing books
+    book* currentBook = books;
+    while (currentBook != NULL) {
+        book* nextBook = currentBook->next;
+        free(currentBook);
+        currentBook = nextBook;
+    }
+
     FILE* file = fopen("books.txt", "r");
     if (file == NULL) {
         printf("Failed to open the file.\n");
@@ -66,49 +90,172 @@ void txttoll(){
 
     // Close the file
     fclose(file);
+}
 
+void deleteBook(char* isbn) {
+    FILE* file = fopen("books.txt", "w");
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        return;
+    }
+
+    book* currentBook = books;
+    while (currentBook != NULL) {
+        if (strcmp(currentBook->isbn, isbn) != 0) {
+            fprintf(file, "%s,%s,%s,%d\n", currentBook->isbn, currentBook->title, currentBook->author, currentBook->publicationYear);
+        }
+        currentBook = currentBook->next;
+    }
+
+    fclose(file);
+
+    // Update the linked list by reading the modified file
+    txttoll();
+}
+
+void addtotxt(char* isbn, char* title, char* author, int publicationYear, int method) {
+    FILE* file = fopen("books.txt", "a+");
+    if (file == NULL) {
+        printf("Failed to open the file.\n");
+        return;
+    }
+
+    if (method == 0) {
+        // Append the book information at the end of the file
+        fprintf(file, "%s,%s,%s,%d\n", isbn, title, author, publicationYear);
+    } else if (method == 1) {
+        // Create a temporary file and write the new book information at the beginning
+        FILE* tempFile = fopen("temp.txt", "w");
+        fprintf(tempFile, "%s,%s,%s,%d\n", isbn, title, author, publicationYear);
+
+        // Append the existing book information from the original file
+        char bookInfo[100];
+        while (fgets(bookInfo, sizeof(bookInfo), file) != NULL) {
+            fprintf(tempFile, "%s", bookInfo);
+        }
+
+        // Close both files
+        fclose(file);
+        fclose(tempFile);
+
+        // Remove the original file and rename the temporary file
+        remove("books.txt");
+        rename("temp.txt", "books.txt");
+
+        // Reopen the file for appending future books
+        file = fopen("books.txt", "a+");
+        if (file == NULL) {
+            printf("Failed to open the file after rearranging.\n");
+            return;
+        }
+    }
+
+    fclose(file);
+}
+
+
+void lltotxt() {
+    book* currentBook = books;
+    FILE* file = fopen("books.txt", "w");
+    while (currentBook != NULL) {
+        fprintf(file, "%s,%s,%s,%d\n", currentBook->isbn, currentBook->title, currentBook->author, currentBook->publicationYear);
+        currentBook = currentBook->next;
+    }
+    fclose(file);
+}
+
+void updateBook(char* isbn, char* feature, char* value) {
+    book* currentbook = books;
+
+    while (currentbook != NULL && strcmp(isbn, currentbook->isbn) != 0) {
+        currentbook = currentbook->next;
+    }
+
+    if (currentbook == NULL) {
+        printf("Book not found.\n");
+        return;
+    }
+
+    if (strcmp(feature, "1") == 0) {
+        strcpy(currentbook->title, value);
+    } else if (strcmp(feature, "2") == 0) {
+        strcpy(currentbook->author, value);
+    } else if (strcmp(feature, "3") == 0) {
+        currentbook->publicationYear = atoi(value);
+    }
+
+    lltotxt();
 }
 
 int main() {
     book* currentBook = books;
     txttoll();
-
+    //printBooks();
     int selection,method;
+    char isbn[15];
+    char title[30];
+    char author[20];
+    int publicationYear;
+    char feature[2];
+    char newvalue[100];
     while(1){
-        char isbn[15] = {'\0'};
-        char title[30]= {'\0'};
-        char author[20] = {'\0'};
-        int publicationYear = 0;
-        printf("enter selection: ");
+        printf("1. Show a student's borrowed books list\n");
+        printf("2. Add book to library\n");
+        printf("3. Delete a book\n");
+        printf("4. Update a book's feature\n");
+        printf("5. Filter and Sort books");
+        printf("9. Terminate the program\n");
+        printf("Enter selection: ");
         scanf("%d",&selection);
         while (getchar() != '\n');
         switch (selection){
         case 1:
+            printf("\nBu part henuz yapilmadi!!\n\n");
+            break;
+        case 2:
             printf("select method (0 or 1): ");
             scanf("%d",&method);
             if(method == 0){
-                printf("enter values:");
-                scanf("%s,%s,%s,%d\n", isbn, title, author, &publicationYear);
+                printf("enter values seperated with comma(,):(ISBN,title,author,publication year):  ");
+                scanf(" %[^,\n],%[^,\n],%[^,\n],%d", isbn, title, author, &publicationYear);
                 while (getchar() != '\n');
                 addBook(isbn, title, author, publicationYear, 0);
+                addtotxt(isbn, title, author, publicationYear,0);
+                //printBooks();
             }
             else if(method == 1){
-                scanf("%[^,],%[^,],%[^,],%d\n", isbn, title, author, &publicationYear);
+                scanf(" %[^,\n],%[^,\n],%[^,\n],%d", isbn, title, author, &publicationYear);
                 addBook(isbn, title, author, publicationYear, 1);    
+                addtotxt(isbn, title, author, publicationYear,1);
+                //printBooks();
             }
             break;
-        case 2:
-            printf("");
+        case 3:
+            printf("\nEnter the ISBN number of the book you want to delete: ");
+            scanf("%s",isbn);
+            deleteBook(isbn);
+            break;
+        case 4:
+            printf("Enter the ISBN of the book you want to change a value of it: ");
+            scanf("%s", isbn);
+            while (getchar() != '\n');  // Clear the input buffer
 
+            printf("\n1. Title\n2. Author\n3. Publication Year\nEnter the feature: ");
+            scanf("%s", feature);
+            printf("Enter the new value for the selected feature: ");
+            scanf(" %[^\n]", newvalue);
+            updateBook(isbn, feature, newvalue);
+            //printBooks();
+            break;
 
         case 9:
-            printf("exitted!!");
+            printf("\nExitted!!\n");
             return 0;
         default:
             break;
         }
     }
-    
+
     
     
     // Free the memory allocated for the books
