082d39cd7d85d4a776e6bee78ace0c2372bda358
ahmethakandemir <hakandemir1625@icloud.com>
2023-06-27 08:18:47 +0300

    .

--- hw12/main.c
+++ hw12/main.c
@@ -137,10 +137,110 @@ void printLinkedList(struct Node* head) {
     }
 }
 
+void serializeLinkedList(struct Node* head) {
+    Node* current = head;
+    FILE* file = fopen("linkedlist.bin", "wb");
+
+    while (current != NULL) {
+        if (strcmp(((Asset1*)current->data)->type, "asset1") == 0) {
+            Asset1* asset = (Asset1*)current->data;
+            fwrite(asset->type, sizeof(asset->type), 1, file);
+            fwrite(asset->ivals, sizeof(asset->ivals), 1, file);
+            fwrite(asset->svals, sizeof(asset->svals), 1, file);
+        } else if (strcmp(((Asset2*)current->data)->type, "asset2") == 0) {
+            Asset2* asset = (Asset2*)current->data;
+            fwrite(asset->type, sizeof(asset->type), 1, file);
+            fwrite(asset->svals, sizeof(asset->svals), 1, file);
+            fwrite(asset->ivals, sizeof(asset->ivals), 1, file);
+        } else if (strcmp(((Asset3*)current->data)->type, "asset3") == 0) {
+            Asset3* asset = (Asset3*)current->data;
+            fwrite(asset->type, sizeof(asset->type), 1, file);
+            fwrite(asset->string1, sizeof(asset->string1), 1, file);
+            fwrite(asset->string2, sizeof(asset->string2), 1, file);
+        } else if (strcmp(((Asset4*)current->data)->type, "asset4") == 0) {
+            Asset4* asset = (Asset4*)current->data;
+            fwrite(asset->type, sizeof(asset->type), 1, file);
+            fwrite(&asset->value1, sizeof(asset->value1), 1, file);
+            fwrite(&asset->value2, sizeof(asset->value2), 1, file);
+            fwrite(&asset->value3, sizeof(asset->value3), 1, file);
+        } else {
+            printf("Unknown asset type.\n");
+        }
+
+        current = current->next;
+    }
+
+    fclose(file);
+}
+
+
+void deserializeLinkedList(struct Node** head) {
+    FILE* file = fopen("linkedlist.bin", "rb");
+
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    while (!feof(file)) {
+        Node* newNode = (Node*)malloc(sizeof(Node));
+        newNode->next = NULL;
+
+        // Read the type of the asset
+        char assetType[20];
+        fread(assetType, sizeof(assetType), 1, file);
+
+        if (strcmp(assetType, "asset1") == 0) {
+            Asset1* newAsset1 = (Asset1*)malloc(sizeof(Asset1));
+            fread(newAsset1->type, sizeof(newAsset1->type), 1, file);
+            fread(newAsset1->ivals, sizeof(newAsset1->ivals), 1, file);
+            fread(newAsset1->svals, sizeof(newAsset1->svals), 1, file);
+            newNode->data = newAsset1;
+        } else if (strcmp(assetType, "asset2") == 0) {
+            Asset2* newAsset2 = (Asset2*)malloc(sizeof(Asset2));
+            fread(newAsset2->type, sizeof(newAsset2->type), 1, file);
+            fread(newAsset2->svals, sizeof(newAsset2->svals), 1, file);
+            fread(newAsset2->ivals, sizeof(newAsset2->ivals), 1, file);
+            newNode->data = newAsset2;
+        } else if (strcmp(assetType, "asset3") == 0) {
+            Asset3* newAsset3 = (Asset3*)malloc(sizeof(Asset3));
+            fread(newAsset3->type, sizeof(newAsset3->type), 1, file);
+            fread(newAsset3->string1, sizeof(newAsset3->string1), 1, file);
+            fread(newAsset3->string2, sizeof(newAsset3->string2), 1, file);
+            newNode->data = newAsset3;
+        } else if (strcmp(assetType, "asset4") == 0) {
+            Asset4* newAsset4 = (Asset4*)malloc(sizeof(Asset4));
+            fread(newAsset4->type, sizeof(newAsset4->type), 1, file);
+            fread(&newAsset4->value1, sizeof(newAsset4->value1), 1, file);
+            fread(&newAsset4->value2, sizeof(newAsset4->value2), 1, file);
+            fread(&newAsset4->value3, sizeof(newAsset4->value3), 1, file);
+            newNode->data = newAsset4;
+        } else {
+            printf("Unknown asset type.\n");
+            free(newNode);
+            continue;
+        }
+
+        if (*head == NULL) {
+            *head = newNode;
+        } else {
+            Node* temp = *head;
+            while (temp->next != NULL) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    fclose(file);
+}
+
 
 int main() {
     struct Node* head = NULL;
     fillLinkedList(&head);
-    printLinkedList(head);
+    //printLinkedList(head);
+    serializeLinkedList(head);
+    deserializeLinkedList(head);
     return 0;
 }
